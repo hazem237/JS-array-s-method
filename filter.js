fd66597f15c1e4225b207efa1a40b5d557f3b449
hazem237 <201082@ppu.edu.ps>
2022-09-17 13:37:01 +0300

    implemented filter method

---
+++ filter.js
@@ -0,0 +1,23 @@
+// The Filter Function 
+function myFilter(arr, fun) {
+    let newArray = [];
+
+    arr.forEach((val) => {
+        if (fun(val))
+            newArray.push(val);
+    })
+
+    return newArray;
+}
+
+//Exmaple of Function that check if the value is falsy value or not
+function falsy_remover(val) {
+    if (Boolean(val))
+        return true;
+
+    return false;
+}
+
+//implementation for the process 
+let arr = [1, 0, undefined, 4, 5, "", null];
+myFilter(arr, falsy_remover);
